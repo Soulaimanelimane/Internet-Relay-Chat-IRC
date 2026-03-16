c97d5b9784bc67bcdd639d85d9a8401ec28abdcb
Brahim Benaali <bbenaali@e3r8p18.1337.ma>
2026-03-16 02:35:04 +0000

    dakchi dayl boot rir dyal copilot

--- server/handel_bot.cpp
+++ server/handel_bot.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handel_bot.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slimane <slimane@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bbenaali <bbenaali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 02:08:16 by slimane           #+#    #+#             */
-/*   Updated: 2026/03/12 03:19:48 by slimane          ###   ########.fr       */
+/*   Updated: 2026/03/13 03:14:57 by bbenaali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,74 @@ void ft_handel_bot(Client &client, std::string &line, std::vector<Client *> &arr
             }
         }
         
+    }
+    if(cmd == "MEMBERS")
+    {
+        str = "NOTE:the members who have @ in their name are admins of this channel \r\n";
+        ft_send(client, str.c_str());
+        for (size_t i = 0; i < channels.size(); i++)
+        {
+            if (channels[i].getmembers().size() > 0)
+            {
+                str = "The members of  this Channel " + channels[i].get_name() + " are :\r\n";
+                ft_send(client, str.c_str());
+                channels[i].List_members(client);       
+            
+            }
+        }
+    }
+    if(cmd == "CHANNELS")
+    {
+        if(channels.size() == 0)
+        {
+            str = "There are no channels available on the server.\r\n";
+            ft_send(client, str.c_str());
+            return;
+        }
+        str = "The available channels are :\r\n";
+        ft_send(client, str.c_str());
+        for (size_t i = 0; i < channels.size(); i++)
+        {
+            str = channels[i].get_name() + "\r\n";
+            ft_send(client, str.c_str());
+        }
+    }
+
+    if(cmd == "USERS")
+    {
+        str = "The available users are :\r\n";
+        ft_send(client, str.c_str());
+        for (size_t i = 0; i < array.size(); i++)
+        {
+            str = array[i]->get_name() + "\r\n";
+            ft_send(client, str.c_str());
+        }
+    }
+    if(cmd == "INFO")
+    {
+        str = "This bot is designed to provide information about the channels and users on the server. It can list the members of each channel, display the available channels, and show the registered users. Use the commands 'BOT LIST', 'BOT CHANNELS', 'BOT USERS', and 'BOT HELP' for more details.\r\n";
+        ft_send(client, str.c_str());
+    }
+    if(cmd == "STATS")
+    {
+        str = "Server Statistics:\r\n";
+        str += "Total Channels: " + std::to_string(channels.size()) + "\r\n";
+        str += "Total Users: " + std::to_string(array.size()) + "\r\n";
+        ft_send(client, str.c_str());
+    }
+    if(cmd == "HELP")
+    {
+        str = "Available BOT Commands:\r\n";
+        str += "BOT LIST - List members of each channel\r\n";
+        str += "BOT CHANNELS - List all available channels\r\n";
+        str += "BOT USERS - List all registered users\r\n";
+        str += "BOT INFO - Get information about the bot\r\n";
+        str += "BOT STATS - Get server statistics\r\n";
+        ft_send(client, str.c_str());
+    }
+    else if (cmd != "LIST" && cmd != "CHANNELS" && cmd != "USERS" && cmd != "INFO" && cmd != "STATS" && cmd != "HELP")
+    {
+        ERR_CMDDISMATCH(cmd);
+        return;
     }
 }
