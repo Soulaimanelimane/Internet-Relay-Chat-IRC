8893a008e64aa0e4d57676ab4471b99f82dc6720
Soulaimanelimane <limansoulaiman@gmail.com>
2026-04-27 15:02:42 +0100

    push to omar

--- ParseSide/kick_cmd.cpp
+++ ParseSide/kick_cmd.cpp
@@ -6,11 +6,22 @@
 /*   By: slimane <slimane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 22:18:59 by omaezzem          #+#    #+#             */
-/*   Updated: 2026/04/25 11:31:36 by slimane          ###   ########.fr       */
+/*   Updated: 2026/04/25 16:40:22 by slimane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ParseSide.hpp"
+
+int ParseSide::check_is_nick(std::string &name)
+{
+    for (size_t i = 0; i < nick.size() ; i++)
+    {
+        if (name == nick[i])
+            return 1;
+    }
+    return 0;
+}
+
 
 void ParseSide::parse_KICK(std::string &cmdarg, std::vector<Channel> &channels, std::vector<Client *> &Clients, Client &cls)
 {
@@ -60,6 +71,11 @@ void ParseSide::parse_KICK(std::string &cmdarg, std::vector<Channel> &channels, 
             ERR_NOSUCHCHANNEL(channelName, cls);
             continue;
         }
+        if (check_is_nick(userList[i]) == 0)
+        {
+            ERR_NOSUCHNICK_INVITE(userList[i],  cls);
+            continue;
+        }
         if (!targetChannel.isUserInChannel(userList[i])){
             ERR_USERNOTINCHANNEL(userList[i], targetChannel.getname(), cls);
             continue;
@@ -78,8 +94,12 @@ void ParseSide::parse_KICK(std::string &cmdarg, std::vector<Channel> &channels, 
         {
             if (it->first == channels[j].get_name())
             {
+                
                 for (size_t k = 0; k < it->second.size(); k++)
-                    channels[j].remove_member(get_client(Clients, it->second[k]) , cls);
+                {
+                    if (check_is_nick(it->second[k]))
+                        channels[j].remove_member(get_client(Clients, it->second[k]) , cls);
+                }
             }
         }
         it++; 
